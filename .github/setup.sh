21113e0882c4c403bf22cf116a788b510771cfcb
Curtis Rueden <ctrueden@wisc.edu>
2022-08-30 14:44:07 -0500

    CI: install libgtk2.0-0
    Apparently it is required by OpenCV's libjniopencv_highgui.

--- .github/setup.sh
+++ .github/setup.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/ci-setup-github-actions.sh
 sh ci-setup-github-actions.sh
+
+# NB: Needed to avoid CI test failure: java.lang.UnsatisfiedLinkError: /home/runner/.javacpp/cache/opencv-4.1.2-1.5.2-linux-x86_64.jar/org/bytedeco/opencv/linux-x86_64/libjniopencv_highgui.so: libgtk-x11-2.0.so.0: cannot open shared object file: No such file or directory
+sudo apt-get -y install libgtk2.0-0
